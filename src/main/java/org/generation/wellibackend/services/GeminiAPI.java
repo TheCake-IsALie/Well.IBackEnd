79f99fec230adca00e36e25ccff48ec050e01bd9
Riccardo <riccardo.zazza@icloud.com>
2025-10-22 18:12:45 +0200

    Test API-Gemini e Creazione Servizio GeminiAPI.java

---
+++ src/main/java/org/generation/wellibackend/services/GeminiAPI.java
@@ -0,0 +1,7 @@
+package org.generation.wellibackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeminiAPI {
+}
--- src/test/java/org/generation/wellibackend/ApplicationTests.java
+++ src/test/java/org/generation/wellibackend/ApplicationTests.java
@@ -3,19 +3,40 @@ package org.generation.wellibackend;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.genai.Client;
+import com.google.genai.types.*;
 import org.generation.wellibackend.model.entities.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
-        User user = new User();
+//        User user = new User();
+//
+//        user.getExtraData().put("firstName", "John");
+//        data.remove("firstName");
 
-        user.getExtraData().put("firstName", "John");
-        //data.remove("firstName");
+        Client client = Client.builder().apiKey("AIzaSyBGULWUJ3wPi8N0p0XQJSUFkflNMUtwFEc").build();
+
+        Content systemInstruction = Content.fromParts(Part.fromText("Sei un sito di news"));
+        Tool googleSearchTool = Tool.builder().googleSearch(GoogleSearch.builder()).build();
+
+        GenerateContentConfig config = GenerateContentConfig.builder()
+                .systemInstruction(systemInstruction)
+                .tools(googleSearchTool)
+                .build();
+
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash-lite", "Mi dai la notizia più importante di oggi?", config);
+
+        // Gets the text string from the response by the quick accessor method `text()`.
+        System.out.println("Unary response: " + response.text());
+
+
 
     }
 
